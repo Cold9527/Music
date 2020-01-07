2a3eba0a30a0c2e6455bd81209e1a8d963209df9
Cold9527 <343707313@qq.com>
2020-01-07 09:15:21 +0800

    playlist bug

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,9 +2,23 @@ import * as types from './mutations-types'
 import {playMode} from 'common/js/config'
 import {shunffle} from 'common/js/util'
 
+
+function findIndex(list,song){
+    return list.findIndex((item) =>{
+        return item.id === song.id
+    })
+}
+
+
 export const selectPlay = function({commit,state},{list,index}) {
     commit(types.SET_SEQUENCE_LIST, list)
-    commit(types.SET_PLAY_LIST, list)
+    if(state.mode === playMode.random){
+        let randomList = shunffle(list)   
+        commit(types.SET_PLAY_LIST, randomList)
+        index = findIndex(randomList, list[index])
+    }else{
+        commit(types.SET_PLAY_LIST, list)
+    }
     commit(types.SET_CURRENT_INDEX, index)
     commit(types.SET_FULL_SCREEN, true)
     commit(types.SET_PLAYING_STATE,true)

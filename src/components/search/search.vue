b9487d8fa7fe3b901f5f1e621e4097207f623521
Cold9527 <343707313@qq.com>
2020-01-09 11:00:10 +0800

    search base create finish

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,16 +1,23 @@
 <template>
-    <div>
-
+    <div class='search'>
+       <div class='search-box-wrapper'>
+          <search-box></search-box>
+       </div>
     </div>
 </template>
 
 <script>
+import  SearchBox from 'base/search-box/search-box'
+
 export default {
    name:'Search',
    data() {
       return {
       }
    },
+   components:{
+      SearchBox
+   }
 
 }
 </script>
